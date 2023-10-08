f63bdbfe3bac71851cc9c56678b4dae3f29a7234
ariflogs <devarifhossain@gmail.com>
2023-10-09 03:25:24 +0600

    removed test file(for now)

--- test/test.js
+++
@@ -1,7 +0,0 @@
-const runner = new SqlToNoSql({
-    srcDBtype: "postgresql",
-    destDBtype: "mongodb",
-    connection: process.env.TEST_MONGODB_URI,
-});
-
-console.log(runner.run("select * from users where role = admin"));
